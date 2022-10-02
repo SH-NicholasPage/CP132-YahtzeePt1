147b418f180665cede7c328f2d33fab37cd41a09
SH-NicholasPage <npage@southhills.edu>
2022-10-02 17:51:55 -0400

    Initial commit

---
+++ YahtzeePt1/YahtzeePt1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+using System;
+
+namespace YahtzeePt1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //TODO: Your code here
+        }
+    }
+}
