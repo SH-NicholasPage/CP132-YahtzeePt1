6f0680340fdb971c8981572913ce5c38a3cad3bc
SH-NicholasPage <npage@southhills.edu>
2022-10-29 22:39:24 -0400

    Added to the starting code

--- YahtzeePt1/YahtzeePt1/Program.cs
+++ YahtzeePt1/YahtzeePt1/Program.cs
@@ -8,9 +8,37 @@ namespace YahtzeePt1
 {
     class Program
     {
+        //Your dice (all values are 0 to start)
+        static int die1, die2, die3, die4, die5, die6;
+
+        //The categories
+        static String catrgoryAces = "aces";
+        static String catrgoryTwos = "twos";
+        static String catrgoryThrees = "threes";
+        static String catrgoryFours = "fours";
+        static String catrgoryFives = "fives";
+        static String catrgorySixes = "sixes";
+
+        //The values categories are worth
+        static int acesWorth = 1;
+        static int twosWorth = 2;
+        static int threesWorth = 3;
+        static int foursWorth = 4;
+        static int fivesWorth = 5;
+        static int sixesWorth = 6;
+
+        //Score
+        static int score = 0;
+
         static void Main()
         {
+            Console.Title = "My Yahtzee game!";
+
             //TODO: Your code here
+
+            Console.WriteLine("Game over! Your final score was " + score);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
